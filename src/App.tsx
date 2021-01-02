de30c95d3bc37301418fbea6aa0fa43b9b1465de
sakito21 <sakito21@gmail.com>
2021-01-02 23:41:02 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,7 @@ import { Sidebar } from "./Sidebar";
 function App() {
   return (
     <Router>
-      {" "}
-      <div className="App">
+      <div className="app">
         <Header />
         <div className="app__body">
           <Sidebar />
--- src/EmailList.tsx
+++ src/EmailList.tsx
@@ -1,10 +1,19 @@
 import { Checkbox, IconButton } from "@material-ui/core";
-import { ArrowDropDown, MoreVert, Redo } from "@material-ui/icons";
+import {
+  ArrowDropDown,
+  ChevronLeft,
+  ChevronRight,
+  KeyboardHide,
+  MoreVert,
+  Redo,
+  Settings,
+} from "@material-ui/icons";
 import { VFC } from "react";
 import "./emailList.css";
+
 export const EmailList = () => {
   return (
-    <div className="emailListq">
+    <div className="emailList">
       <div className="emailList__settings">
         <div className="emailList__settingsLeft">
           <Checkbox />
@@ -18,7 +27,23 @@ export const EmailList = () => {
             <MoreVert />
           </IconButton>
         </div>
+        <div className="emailList__settingsRight">
+          <IconButton>
+            <ChevronLeft />
+          </IconButton>
+          <IconButton>
+            <ChevronRight />
+          </IconButton>
+          <IconButton>
+            <KeyboardHide />
+          </IconButton>
+          <IconButton>
+            <Settings />
+          </IconButton>
+        </div>
       </div>
+
+      <div className="emailList__sections"></div>
     </div>
   );
 };
